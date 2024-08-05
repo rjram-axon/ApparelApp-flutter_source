238351a8ebf8d1519bb640e2f956e47882c99af5
Jeyaram <jeyaramaxon@gmail.com>
2024-08-05 16:53:44 +0530

    PurchaseQuotation Attachment add

--- lib/management/purchasequote_approve.dart
+++ lib/management/purchasequote_approve.dart
@@ -1,8 +1,10 @@
 import 'dart:convert';
+import 'dart:io';
 import 'package:apparelapp/management/purchasequotedetails.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:another_flushbar/flushbar.dart';
+import 'package:image_picker/image_picker.dart'; // Import image_picker package
 
 class PurchaseQuotationEditPage extends StatefulWidget {
   final int quoteId;
@@ -19,6 +21,9 @@ class _PurchaseQuotationEditPageState extends State<PurchaseQuotationEditPage> {
   bool _isLoading = true;
   String? _errorMessage;
   bool _isApproved = false; // Track approval status
+  final TextEditingController _attachmentController = TextEditingController(); // Controller for the attachment text box
+  final ImagePicker _picker = ImagePicker(); // ImagePicker instance
+  File? _selectedImage; // To hold the selected image file
 
   @override
   void initState() {
@@ -75,6 +80,25 @@ class _PurchaseQuotationEditPageState extends State<PurchaseQuotationEditPage> {
     }
   }
 
+  Future<void> _pickImage() async {
+    try {
+      final pickedFile = await _picker.pickImage(source: ImageSource.gallery);
+
+      if (pickedFile != null) {
+        setState(() {
+          _selectedImage = File(pickedFile.path);
+          _attachmentController.text = _selectedImage!.path.split('/').last;
+        });
+      }
+    } catch (e) {
+      _showFlushbar(
+        'Failed to pick image: $e',
+        Colors.red,
+        Icons.error,
+      );
+    }
+  }
+
   void _handleApproval(String action) async {
     final apiUrl =
         'http://13.232.84.26:81/api/updatepurchasequoteapproval/${widget.quoteId}';
@@ -86,8 +110,7 @@ class _PurchaseQuotationEditPageState extends State<PurchaseQuotationEditPage> {
           'Content-Type': 'application/json; charset=UTF-8',
         },
         body: jsonEncode({
-          'isApproved':
-              action, // Set action to 'A' for approve and 'P' for reject
+          'isApproved': action, // Set action to 'A' for approve and 'P' for reject
         }),
       );
 
@@ -226,25 +249,42 @@ class _PurchaseQuotationEditPageState extends State<PurchaseQuotationEditPage> {
                         ),
                         Padding(
                           padding: const EdgeInsets.all(16.0),
-                          child: ElevatedButton.icon(
-                            onPressed: () {
-                              _handleApproval(_isApproved ? 'P' : 'A');
-                            },
-                            icon: Icon(
-                              _isApproved ? Icons.undo : Icons.check,
-                            ),
-                            label: Text(
-                              _isApproved ? 'Revert Items' : 'Approve Items',
-                            ),
-                            style: ElevatedButton.styleFrom(
-                              primary: _isApproved ? Colors.red : Colors.green,
-                              onPrimary: Colors.white,
-                              padding: EdgeInsets.symmetric(
-                                  vertical: 12.0, horizontal: 24.0),
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(30.0),
+                          child: Column(
+                            children: [
+                              TextField(
+                                controller: _attachmentController,
+                                readOnly: true,
+                                decoration: InputDecoration(
+                                  labelText: 'Attachment',
+                                  suffixIcon: IconButton(
+                                    icon: Icon(Icons.attach_file),
+                                    onPressed: _pickImage,
+                                  ),
+                                  border: OutlineInputBorder(),
+                                ),
                               ),
-                            ),
+                              SizedBox(height: 16),
+                              ElevatedButton.icon(
+                                onPressed: () {
+                                  _handleApproval(_isApproved ? 'P' : 'A');
+                                },
+                                icon: Icon(
+                                  _isApproved ? Icons.undo : Icons.check,
+                                ),
+                                label: Text(
+                                  _isApproved ? 'Revert Items' : 'Approve Items',
+                                ),
+                                style: ElevatedButton.styleFrom(
+                                  primary: _isApproved ? Colors.red : Colors.green,
+                                  onPrimary: Colors.white,
+                                  padding: EdgeInsets.symmetric(
+                                      vertical: 12.0, horizontal: 24.0),
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(30.0),
+                                  ),
+                                ),
+                              ),
+                            ],
                           ),
                         ),
                         SizedBox(height: 16),
